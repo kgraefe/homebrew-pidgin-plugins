c4f30ffb11238ed7ccb1b61e266d1b6d98b69c2e
Konrad Gräfe <konradgraefe@aol.com>
2019-12-20 09:23:36 +0100

    Update pidgin-znc-helper
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

--- Formula/pidgin-znc-helper.rb
+++ Formula/pidgin-znc-helper.rb
@@ -1,8 +1,8 @@
 class PidginZncHelper < Formula
   desc "ZNC Helper uses message timestamp from a ZNC Bouncer and sets them within Pidgin so that replayed messages will be displayed with the correct timestamp."
   homepage "https://github.com/kgraefe/pidgin-znc-helper"
-  url "https://github.com/kgraefe/pidgin-znc-helper/releases/download/v1.8/pidgin-znc-helper-1.8.tar.gz"
-  sha256 "3a27fdcb2ed90c8c32582ad6dfeb4cc9110ff33b365c7c380b0bb6a7155d15cf"
+  url "https://github.com/kgraefe/pidgin-znc-helper/releases/download/v1.9/pidgin-znc-helper-1.9.tar.gz"
+  sha256 "9ba711b8cae512f06915ea2fa9cb435a93f46945f6b7ad82678b349cccc8e020"
 
   depends_on "pidgin"
   depends_on "intltool" => :build
