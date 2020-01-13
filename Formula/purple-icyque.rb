9634241781e0f721b9317f324d7e1071111aa70a
Konrad Gräfe <konradgraefe@aol.com>
2020-01-13 14:54:20 +0100

    Update purple-icyque
    Signed-off-by: Konrad Gräfe <konradgraefe@aol.com>

--- Formula/purple-icyque.rb
+++ Formula/purple-icyque.rb
@@ -2,10 +2,10 @@ class PurpleIcyque < Formula
   desc "ICQ WIM protocol for libpurple"
   homepage "https://github.com/EionRobb/icyque"
   version "git"
-  revision 4
+  revision 5
 
   url "https://github.com/EionRobb/icyque.git",
-    :revision => "bc266be8e891cbbd7466d901c06d1c57e327ceb9"
+    :revision => "ebc522743e98a6b9e1fa0703703fa5298862b60a"
 
   depends_on "pidgin"
   depends_on "json-glib"
